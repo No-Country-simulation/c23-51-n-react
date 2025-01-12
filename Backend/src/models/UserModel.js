e75e774f7d107a8d0f9650629c7e6baa38d7ac8d
tony <anthony.z.roldan@gmail.com>
2025-01-12 17:07:47 -0500

    feat(user): implement login user

--- Backend/src/models/UserModel.js
+++ Backend/src/models/UserModel.js
@@ -3,11 +3,45 @@ class userModel {
     this.db = db
   }
 
-  async createUser ({ email, password }) {
+  async createUser ({ email, password, name, brithdate, height, weigth }) {
     try {
       const pool = this.db
-      const [result] = await pool.query('INSERT INTO users SET email = ?, password = ?', [email, password])
-      return +result.affectedRows
+      const [user] = await pool.query('INSERT INTO users SET email = ?, password = ?', [email, password])
+      const userId = +user.insertId
+
+      if (userId) {
+        await pool.query(`INSERT INTO profiles 
+          SET
+            user_id = ?,
+            name = ?,
+            birthdate = ?,
+            height = ?,
+            weight = ?`,
+        [userId, name, brithdate, height, weigth])
+      }
+
+      return +user.affectedRows
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  }
+
+  async findByEmail (email) {
+    try {
+      const pool = this.db
+      const [[user]] = await pool.query(`SELECT 
+          id,
+          email,
+          password,
+          role_id,
+          status 
+        FROM users 
+        WHERE email = ?`,
+      [email])
+
+      if (!user) return {}
+      return user
     } catch (error) {
       console.log(error)
       throw error

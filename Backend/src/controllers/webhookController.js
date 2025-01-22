780a1941c049a873d921a4fe2068895066774577
tony <anthony.z.roldan@gmail.com>
2025-01-21 22:34:19 -0500

    feat(suscription): create process suscription with paypal, return link payment and use the webhook to capture update suscriptions

--- Backend/src/controllers/webhookController.js
+++ Backend/src/controllers/webhookController.js
@@ -1,12 +1,37 @@
+const logger = require('../utils/logger')
+
 class webhookController {
-  constructor (userModel) {
-    this.userModel = userModel
+  constructor (suscriptionModel) {
+    this.suscriptionModel = suscriptionModel
   }
 
   async handleWebHook (req, res) {
-    console.log(await req.body)
+    try {
+      const event = req.body
 
-    res.send('correcto')
+      switch (event.event_type) {
+        case 'BILLING.SUBSCRIPTION.ACTIVATED':
+        {
+          const { id, status, start_time, plan_id, custom_id } = event.resource
+          const data = {
+            id,
+            status,
+            create_time: start_time,
+            plan_id,
+            custom_id
+          }
+          await this.suscriptionModel.createSuscription(data)
+          return res.status(200).send('Webhook suscription processed successfully')
+        }
+
+        default:
+          console.log('nada')
+          break
+      }
+    } catch (error) {
+      logger.error('Error processing webhook:', error)
+      return res.status(500).send('Internal Server Error')
+    }
   }
 }
 

2ce517d6365490ca5bf9d23d91f54530fac13fa9
john cotamo <johncotamo@hotmail.com>
2025-01-21 22:52:47 -0500

    se intenta configurar por medio de grid

--- Frontend/src/components/ContactUs.jsx
+++ Frontend/src/components/ContactUs.jsx
@@ -3,24 +3,40 @@ import { men, routine, woman } from "@/assets";
 export const ContactUs = () => {
   return (
     <>
-    <div className="flex  m-0">
-
-
-      <div className="container mx-auto ">
-        <div className="flex gap-4 ">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 py-8 mt-20">
+        {/* Sección de imágenes: mujer y hombre */}
+        <div className="flex justify-center items-center">
           <img
-            className="w-[18%] h-auto rounded-lg border border-limeBords"
+            className="w-full h-auto rounded-lg border border-limeBords mb-auto"
             src={woman}
-            alt="woman"
+            alt="Mujer haciendo ejercicio"
           />
-          <img className="w-[18%] h-auto rounded-lg border border-limeBords" src={men} alt="men" />
         </div>
-        <div className="p-8 mt-4 font-semibold text-black rounded-lg bg-limeGreen w-[37%] min-h-[30vh] rounded-lg">
-          <h2 className="text-2xl font-bold">
+        <div className="flex justify-center items-center">
+          <img
+            className="w-full h-auto rounded-lg border border-limeBords mb-auto"
+            src={men}
+            alt="Hombre haciendo ejercicio"
+          />
+        </div>
+
+        {/* Imagen de rutina  */}
+        <div className="flex justify-center items-center col-span-2 lg:col-span-1">
+          <img
+            className="w-full h-auto rounded-lg border border-limeBords"
+            src={routine}
+            alt="Rutina de ejercicios"
+          />
+        </div>
+
+        {/* Contenedor del texto y los botones */}
+        <div className="col-span-1 md:col-span-2 lg:col-span-2 p-8 mt-4 font-semibold text-black rounded-lg bg-limeGreen w-[%] h-[90%] ">
+          <h2 className="text-2xl font-bold mb-4">
             "Ponte en forma, fortalécete, y obtén los resultados que anhelas con nuestro plan de
             fitness científicamente diseñado."
           </h2>
-          <ul className="font-semibold space-y-2 mt-4">
+
+          <ul className="font-semibold space-y-2 mb-4">
             <li className="flex items-center space-x-2">
               <span className="flex items-center justify-center w-6 h-6 rounded-full bg-black text-white text-sm">
                 ✓
@@ -41,7 +57,7 @@ export const ContactUs = () => {
             </li>
           </ul>
 
-          <div className="space-y-4">
+          <div className="space-y-4 mt-4">
             <button className="px-8 py-2 font-semibold border border-gray-800 text-black transition rounded-full bg-limeGreen hover:bg-fondo hover:text-limeGreen">
               Quiero ser parte
             </button>
@@ -51,10 +67,6 @@ export const ContactUs = () => {
           </div>
         </div>
       </div>
-      <div className="w-[18%] h-auto">
-        <img src={routine} alt="routine" />
-      </div>
-    </div>
     </>
   );
 };

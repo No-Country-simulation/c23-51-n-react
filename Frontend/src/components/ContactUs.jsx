be572ca92ae5c7a7a4b25a7c5960abb4ff4f805e
john cotamo <johncotamo@hotmail.com>
2025-01-21 19:39:33 -0500

    se raliza cambio en barra

--- Frontend/src/components/ContactUs.jsx
+++ Frontend/src/components/ContactUs.jsx
@@ -3,32 +3,58 @@ import { men, routine, woman } from "@/assets";
 export const ContactUs = () => {
   return (
     <>
-      <div className="container mx-auto">
+    <div className="flex  m-0">
+
+
+      <div className="container mx-auto ">
         <div className="flex gap-4 ">
-          <img className="h-auto w-18" src={woman} alt="woman" />
-          <img className="h-auto w-18" src={men} alt="men" />
+          <img
+            className="w-[18%] h-auto rounded-lg border border-limeBords"
+            src={woman}
+            alt="woman"
+          />
+          <img className="w-[18%] h-auto rounded-lg border border-limeBords" src={men} alt="men" />
         </div>
-        <div className="p-8 mt-4 font-semibold text-black rounded-lg bg-limeGreen">
+        <div className="p-8 mt-4 font-semibold text-black rounded-lg bg-limeGreen w-[37%] min-h-[30vh] rounded-lg">
           <h2 className="text-2xl font-bold">
             "Ponte en forma, fortalécete, y obtén los resultados que anhelas con nuestro plan de
             fitness científicamente diseñado."
           </h2>
-          <ul>
-            <li>Incrementa músculo y fuerza</li>
-            <li>Siéntete más sano</li>
-            <li>Entrena a tu ritmo</li>
+          <ul className="font-semibold space-y-2 mt-4">
+            <li className="flex items-center space-x-2">
+              <span className="flex items-center justify-center w-6 h-6 rounded-full bg-black text-white text-sm">
+                ✓
+              </span>{" "}
+              <span>Incrementa músculo y fuerza</span>
+            </li>
+            <li className="flex items-center space-x-2">
+              <span className="flex items-center justify-center w-6 h-6 rounded-full bg-black text-white text-sm">
+                ✓
+              </span>{" "}
+              <span>Siéntete más sano</span>
+            </li>
+            <li className="flex items-center space-x-2">
+              <span className="flex items-center justify-center w-6 h-6 rounded-full bg-black text-white text-sm">
+                ✓
+              </span>{" "}
+              <span>Entrena a tu ritmo</span>
+            </li>
           </ul>
+
+          <div className="space-y-4">
+            <button className="px-8 py-2 font-semibold border border-gray-800 text-black transition rounded-full bg-limeGreen hover:bg-fondo hover:text-limeGreen">
+              Quiero ser parte
+            </button>
+            <button className="px-8 py-2 font-semibold border border-gray-800 text-black transition rounded-full bg-limeGreen hover:bg-fondo hover:text-limeGreen">
+              Contáctanos
+            </button>
+          </div>
         </div>
-        <button className="px-8 py-2 mt-4 font-semibold text-black transition rounded-full bg-limeGreen hover:bg-limonGreen2">
-          Quiero ser parte
-        </button>
-        <button className="px-8 py-2 mt-4 font-semibold text-black rounded-full bg-limeGreen hover:bg-limeGreen2">
-          Contáctanos
-        </button>
       </div>
-      <div>
+      <div className="w-[18%] h-auto">
         <img src={routine} alt="routine" />
       </div>
+    </div>
     </>
   );
 };
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,36 +1,44 @@
+import { useState } from "react";
+
 export const Navbar = () => {
+  
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className="bg-fondo  border border-gray-800  ">
-      <div className="container mx-auto flex items-center justify-between px-4 py-3">
+    <nav className="bg-fondo border border-gray-800 rounded-full mt-9 w-auto h-[50%] mb-8">
+      <div className="container mx-auto flex items-center justify-between px-4 py-3 ">
         {/* Logo */}
         <a className="text-xl font-bold text-fondo" href="/">
           Inicio
         </a>
 
         {/* Menu principal */}
-        <div className="hidden md:flex space-x-6">
+        <div className={`hidden md:flex space-x-6 ${isMenuOpen ? "block" : "hidden"} md:block`}>
           <a
-            className="px-4 py-2   border-gray-700 text-gray-300 hover:bg-slate-600 hover:text-gray-300 transition rounded-2xl font-semibold"
+            className="px-4 py-2 border-gray-700 text-gray-300 hover:bg-slate-600 hover:text-gray-300 transition rounded-2xl font-semibold"
             href="/"
           >
             Inicio
           </a>
           <a
             href="#"
-            className="px-4 py-2   border-gray-700 text-gray-300 hover:bg-slate-600 hover:text-gray-300 transition rounded-2xl font-semibold"
+            className="px-4 py-2 border-gray-700 text-gray-300 hover:bg-slate-600 hover:text-gray-300 transition rounded-2xl font-semibold"
           >
             Planes
           </a>
           <a
             href="#"
-            className= "px-4 py-2   border-gray-700 text-gray-300 hover:bg-slate-600 hover:text-gray-300 transition rounded-2xl font-semibold"
+            className="px-4 py-2 border-gray-700 text-gray-300 hover:bg-slate-600 hover:text-gray-300 transition rounded-2xl font-semibold"
           >
             Rutinas
           </a>
           <a
             href="#"
-            // className="text-gray-300 hover:text-gray-100  transition-colors font-semibold"
-            className= "px-4 py-2   border-gray-700 text-gray-300 hover:bg-slate-600 hover:text-gray-300 transition rounded-2xl font-semibold"
+            className="px-4 py-2 border-gray-700 text-gray-300 hover:bg-slate-600 hover:text-gray-300 transition rounded-2xl font-semibold"
           >
             Comunidad
           </a>
@@ -40,13 +48,13 @@ export const Navbar = () => {
         <div className="hidden md:flex space-x-4">
           <a
             href="/register"
-            className=" px-4 py-2  border-gray-700 text-gray-300 hover:bg-limeGreen hover:text-gray-900 transition rounded-2xl font-semibold"
+            className="px-4 py-2 border-gray-700 text-gray-300 hover:bg-limeGreen hover:text-gray-900 transition rounded-2xl font-semibold"
           >
             Registrarse
           </a>
           <a
             href="/login"
-            className=" px-4 py-2  border-gray-700 text-gray-300 hover:bg-limeGreen hover:text-gray-900 transition rounded-2xl font-semibold"
+            className="px-4 py-2 border-gray-700 text-gray-300 hover:bg-limeGreen hover:text-gray-900 transition rounded-2xl font-semibold"
           >
             Iniciar sesión
           </a>
@@ -56,6 +64,7 @@ export const Navbar = () => {
         <button
           className="block md:hidden text-gray-700 hover:text-gray-900 focus:outline-none"
           aria-label="Toggle menu"
+          onClick={toggleMenu}
         >
           <svg
             className="w-6 h-6"
@@ -72,6 +81,50 @@ export const Navbar = () => {
             />
           </svg>
         </button>
+      </div>
+
+      {/* Menú hamburguesa visible solo en dispositivos móviles */}
+      <div className={`md:hidden ${isMenuOpen ? "block" : "hidden"} bg-gray-800 text-white py-4`}>
+        <a
+          href="/"
+          className="block px-4 py-2 text-gray-300 hover:bg-slate-600"
+        >
+          Inicio
+        </a>
+        <a
+          href="#"
+          className="block px-4 py-2 text-gray-300 hover:bg-slate-600"
+        >
+          Planes
+        </a>
+        <a
+          href="#"
+          className="block px-4 py-2 text-gray-300 hover:bg-slate-600"
+        >
+          Rutinas
+        </a>
+        <a
+          href="#"
+          className="block px-4 py-2 text-gray-300 hover:bg-slate-600"
+        >
+          Comunidad
+        </a>
+
+        {/* Botones de móvil */}
+        <div className="space-y-4">
+          <a
+            href="/register"
+            className="block px-4 py-2 text-gray-300 hover:bg-limeGreen hover:text-gray-900"
+          >
+            Registrarse
+          </a>
+          <a
+            href="/login"
+            className="block px-4 py-2 text-gray-300 hover:bg-limeGreen hover:text-gray-900"
+          >
+            Iniciar sesión
+          </a>
+        </div>
       </div>
     </nav>
   );
